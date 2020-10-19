41d91caa1b5dcb4a2a3c42e3a29b96b035fd2ea9
DanaDovzh <danadovzh16@gmail.com>
2020-10-19 17:29:43 +0300

    Create .sass file

--- Form/js/main.js
+++ Form/js/main.js
@@ -1,5 +1,6 @@
 window.onload = function(e) {
     let error  = false;
+    let emailValNot = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let inputs = document.querySelectorAll(".form__text--check");
     let messagePass =  document.querySelector("#message-confirm-pass");
     let password = document.querySelector("input[name='password']");
@@ -10,33 +11,51 @@ window.onload = function(e) {
             if(inputs[i].value === "") {
                 inputs[i].classList.add("form__text--err");
                 error = true;
+                console.log("1");
             } else if ((inputs[i].name === "first-name" || inputs[i].name === "last-name")) {
                 if(inputs[i].value.search(/\d/) != -1) {
                     alert(`Find number in \"${inputs[i].placeholder}\"`);
                     inputs[i].classList.add("form__text--err");
+                    error = true;
+                    console.log("2");
                 } else {
                     inputs[i].classList.remove("form__text--err");
-                    alert(`OK in \"${inputs[i].placeholder}\"`);
+                    console.log("3");
                 }
-                } 
-                else if(password.value != confirmPassword.value) {
-                    messagePass.innerHTML = "Passwords do not match";
-                    messagePass.classList.remove("form__text-pass--good");
-                    messagePass.classList.add("form__text-pass--err");
+            } else if(inputs[i].name ==="email"){
+                if(inputs[i].value.search(emailValNot) === -1) {
+                    document.querySelector("#message-email").innerHTML = "  Invalid email";
+                    document.querySelector("#message-email").classList.add("form__text-invalid--err");
                     inputs[i].classList.add("form__text--err");
-                } else if(password.value === confirmPassword.value){
-                    messagePass.innerHTML = "Passwords match";
-                    messagePass.classList.remove("form__text-pass--err");
-                    messagePass.classList.add("form__text-pass--good");
+                    error = true;
+                    console.log("4");
+                } else {
+                    document.querySelector("#message-email").innerHTML = "  Invalid email";
+                    document.querySelector("#message-email").classList.remove("form__text-invalid--err");
+                    document.querySelector("#message-email").classList.add("form__text-invalid--good");
                     inputs[i].classList.remove("form__text--err");
-                } 
-                else {
-                    error = false;
-                    // inputs[i].classList.remove("form__text--err");
+                    console.log("5");
                 }
+            } else if(password.value != confirmPassword.value) {
+                messagePass.innerHTML = "  Passwords do not match";
+                messagePass.classList.remove("form__text-invalid--good");
+                messagePass.classList.add("form__text-invalid--err");
+                inputs[i].classList.add("form__text--err");
+                error = true;
+                console.log("6");
+            } else if(password.value === confirmPassword.value){
+                messagePass.innerHTML = "  Passwords match";
+                messagePass.classList.remove("form__text-invalid--err");
+                messagePass.classList.add("form__text-invalid--good");
+                inputs[i].classList.remove("form__text--err");
+                console.log("7");
+            } else {
+                error = false;
+                console.log("8");
+            }
             
         }
-
+        console.log(error);
         if(error) 
             e.preventDefault();
         else
