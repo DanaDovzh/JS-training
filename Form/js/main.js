71e84cb661df50f4b3b38d12224641d706f3c846
DanaDovzh <danadovzh16@gmail.com>
2020-10-23 17:29:06 +0300

    Add function for error

--- Form/js/main.js
+++ Form/js/main.js
@@ -1,76 +1,69 @@
 window.onload = function(e) {
+
+        document.body.classList.add('loaded_hiding');
+        window.setTimeout(function () {
+          document.body.classList.add('loaded');
+          document.body.classList.remove('loaded_hiding');
+        }, 500);
+      
     let error  = false;
     let countWithoutEr = 0;
+    let message;
     let emailValNot = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let inputs = document.querySelectorAll(".form__text--check");
     let messages = document.querySelectorAll(".form__message-check");
     let messagePass =  document.querySelector("#message-confirm-pass");
     let password = document.querySelector("input[name='password']");
     let confirmPassword = document.querySelector("input[name='confirm-password']");
+
+    function isError(message, i) {
+        messages[i].innerHTML = message;
+        messages[i].classList.remove("form__message-check--good");
+        messages[i].classList.add("form__message-check--err");
+        inputs[i].classList.add("form__text--err");
+        error = true;
+    }
+
+    function notError(message, i) {
+        inputs[i].classList.remove("form__text--err");
+        messages[i].innerHTML = "  &#10004";
+        messages[i].classList.remove("form__message-check--err");
+        messages[i].classList.add("form__message-check--good");
+    }
     document.querySelector('form').onsubmit = function (e) {
+        error  = false;
         for (let i = 0; i < inputs.length; i++) {
-            countWithoutEr = 0;
+            
             if(inputs[i].value === "") {
                 inputs[i].classList.add("form__text--err");
                 messages[i].classList.add("form__message-check--err");
                 messages[i].innerHTML = " &#10007;";
+                console.log(error, "1");
                 error = true;
-                console.log("1");
             } else if ((inputs[i].name === "first-name" || inputs[i].name === "last-name")) {
                 if(inputs[i].value.search(/\d/) != -1) {
-                    messages[i].innerHTML = "  Enter without numbers";
-                    messages[i].classList.remove("form__message-check--good");
-                    messages[i].classList.add("form__message-check--err");
-                    inputs[i].classList.add("form__text--err");
-                    error = true;
-                    console.log("2");
+                    isError("  Enter without numbers", i);
+                    console.log(error, "2");
                 } else {
-                    inputs[i].classList.remove("form__text--err");
-                    messages[i].innerHTML = "  &#10004";
-                    messages[i].classList.remove("form__message-check--err");
-                    messages[i].classList.add("form__message-check--good");
-                    console.log("3");
-                    
-                    countWithoutEr++;
+                    notError("  &#10004", i);
+                    console.log(error, "3");
                 }
             } else if(inputs[i].name ==="email"){
                 if(inputs[i].value.search(emailValNot) === -1) {
-                    document.querySelector("#message-email").innerHTML = "  Invalid email";
-                    messages[i].classList.remove("form__message-check--good");
-                    messages[i].classList.add("form__message-check--err");
-                    inputs[i].classList.add("form__text--err");
-                    error = true;
-                    console.log("4");
+                    isError("  Invalid email", i);
+                    console.log(error, "4");
                 } else {
-                    document.querySelector("#message-email").classList.remove("form__message-check--err");
-                    document.querySelector("#message-email").classList.add("form__message-check--good");
-                    inputs[i].classList.remove("form__text--err");
-                    console.log("5");
-                    messages[i].innerHTML = " &#10004;";
-                    messages[i].classList.remove("form__message-check--err");
-                    messages[i].classList.add("form__message-check--good");
-                    countWithoutEr++;
+                    notError("  &#10004", i);
+                    console.log(error, "5");
                 }
             } else if(password.value !== confirmPassword.value) {
-                messagePass.innerHTML = "  Passwords do not match";
-                messagePass.classList.remove("form__message-check--good");
-                messagePass.classList.add("form__message-check--err");
-                inputs[i].classList.add("form__text--err");
-                error = true;
-                console.log("6");
+                isError("  Passwords do not match", i);
+                console.log(error, "6");
             } else if(password.value === confirmPassword.value){
-                messages[i].innerHTML = "  &#10004;";
-                messages[i].classList.remove("form__message-check--err");
-                messages[i].classList.add("form__message-check--good");
-                inputs[i].classList.remove("form__text--err");
-                console.log("7");
-                console.log("countWithoutEr ", countWithoutEr);
-            } else {
-                error = false;
-                console.log("8");
+                notError("  &#10004", i);
+                console.log(error, "7");
             }
-            
-        }
+        };
         console.log(error);
         document.querySelector("input[type='reset']").addEventListener('click', function(){
             for(let i = 0; i < inputs.length; i++) {
@@ -79,7 +72,7 @@ window.onload = function(e) {
                 inputs[i].classList.remove("form__text--err");
                 messages[i].innerHTML = "";
             }
-        })
+        });
         if(error) 
             e.preventDefault();
         else
